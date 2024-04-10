3ca66b95945f05b6fb64500ff0a2c2d956412fc8
0940185778 <biniyamassefa648@gmail.com>
2024-04-10 10:44:09 +0300

    list-comprehensions

---
+++ list-comprehensions.py
@@ -0,0 +1,9 @@
+import itertools
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    result = [list(t) for t in itertools.product(range(x + 1), range(y + 1), range(z + 1)) if sum(t) != n]
+    print(result)
+  
